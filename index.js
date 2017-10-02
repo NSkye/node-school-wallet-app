5d53835e1904a0e241f105f4248f155778cdfc9b
nikolai-laevskii <nikolai.laevskii@yandex.ru>
2017-10-02 08:29:41 +0300

    Метод Get Cards и Алгоритм "Луна""

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const luhn = (num) => {
+	console.log('Checking card number: '+num);
+	num = num.split('').reverse();
+	let i = 1;
+	console.log('Check-digit: '+num[0]);
+	while (num[i]) {
+		if (i%2===0) {
+			let newNum = (Number(num[i])*2).toString();
+			if (newNum.length>1)
+				newNum = (Number(newNum[0])+Number(newNum[1])).toString();
+			num[i]=newNum;
+		}
+	i++;
+	}
+	num = num.reduce((a, b) => Number(a)+Number(b));
+	console.log('Final sum: '+num);
+	if (num%10)
+		return false;
+	else
+		return true;
+}
 
 app.use(express.static('public'));
 
@@ -27,6 +50,30 @@ app.get('/transfer', (req, res) => {
 		from,
 		to
 	});
+});
+
+app.get('/cards', (req, res) => {
+	fs.open('source/cards.json', 'r', (err, fd) => {
+		if (err) throw err;
+		fs.readFile(fd, 'utf8', (err, data) => {
+			if (err) throw err;
+			res.json(JSON.parse(data));
+			fs.close(fd, () => {console.log('file closed')});
+		});
+	});
+});
+
+app.use(bodyParser.json());
+
+app.post('/cards', (req, res)=>{
+	const card = req.body;
+	const validCard = card && card.cardNumber && /^[0-9]+$/.test(card.cardNumber) && luhn(card.cardNumber) && card.balance && /^[0-9]+$/.test(card.balance);
+	if (!validCard) {
+		res.statusCode = 400;
+		return res.end('400 Bad request');
+	} else {
+		res.json(req.body);
+	}
 });
 
 app.listen(3000, () => {
